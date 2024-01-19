1e17dd54f779487c81fce74fd54f71b19c0101d5
Callummmmmmm <callum.f.wilson@hotmail.com>
2024-01-19 17:38:03 +0000

    Data Reading .py Update

---
+++ data_reading.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import IPython.display as ipd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+lep_author_data = pd.read_csv('/content/drive/MyDrive/Group 3/authors.leptospirosis.csv')
+
